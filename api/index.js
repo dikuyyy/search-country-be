f0e4684b3f5360c4036263f73665d54c72bc09fb
dikuyyy <andikaaddobe@gmail.com>
2024-05-02 22:25:35 +0700

    update: refactor app.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
+const index = express();
 const port = 3000;
 
-const data = require('./countriesV2.json');
+const data = require('../countriesV2.json');
 
-app.use('/images', express.static('public'));
-app.get('/name/:name', (req, res) => {
+index.use('/images', express.static('public'));
+index.get('/name/:name', (req, res) => {
     const name = req.params.name;
     const isFullName = req.query.fullName === 'true';
     const filteredCountries = data.filter(country =>
@@ -31,7 +31,7 @@ app.get('/name/:name', (req, res) => {
     }
 })
 
-app.get('/callingcode/:code', (req, res) => {
+index.get('/callingcode/:code', (req, res) => {
     const code = req.params.code;
     const filteredCountries = data.filter(country => country.callingCodes.includes(code));
     if (filteredCountries.length > 0) {
@@ -41,7 +41,7 @@ app.get('/callingcode/:code', (req, res) => {
     }
 })
 
-app.get('/currency/:currencies', (req, res) => {
+index.get('/currency/:currencies', (req, res) => {
     const currencies = req.params.currencies;
     const filteredCountries = data.filter(country => country.currencies.some(currency => currency.code === currencies));
     const response = filteredCountries.map((item) => {
@@ -59,6 +59,6 @@ app.get('/currency/:currencies', (req, res) => {
 
 
 
-app.listen(port, () => {
+index.listen(port, () => {
     console.log(`API running on http://localhost:${port}`);
 });
